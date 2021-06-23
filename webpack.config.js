6299cbd9d299b0bf2242c3453b50c69676fc8a05
anton-varenyuk <anton.varenyuk@avenga.com>
2021-06-23 20:31:32 +0300

    worker2: some edits

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,9 @@ const path = require('path');
 module.exports = {
   entry: {
     main: './src/index.js',
-    // worker: './src/app/worker.js'
   },
   output: {
-    // publicPath: '',
-    // path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: "main.js",
+    path: path.resolve(__dirname, 'dist')
   },
 };
