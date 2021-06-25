c18537cab27d255c31ab17e46dbf855b7bf2b61c
anton-varenyuk <anton.varenyuk@avenga.com>
2021-06-25 20:40:06 +0300

    worker2: IDB added to chat

--- src/app/services/message.service.js
+++ src/app/services/message.service.js
@@ -1,22 +1,30 @@
 import MessageComponent from "../components/message.component";
+import IDBService from "./idb.service";
 const moment = require('moment');
 
 export default class MessageService {
     constructor() {
+        this.db = new IDBService('messages', 'messages');
         this.chatRef = document.getElementById('chat-box');
         this.inputRef = document.getElementById('message-input');
 
+        this.renderMessages();
         this.watchInput();
     }
 
-    renderMessage(text, time) {
-        this.chatRef.appendChild(new MessageComponent(text, time).template());
+    renderMessage(message, time) {
+        this.chatRef.appendChild(new MessageComponent(message, time).template());
     }
 
     watchInput() {
         this.inputRef.addEventListener('keydown', (e) => {
-            if ((e.code === 'Enter' || e.code === 'NumpadEnter') && this.inputRef.value !== '') {
-                this.renderMessage(this.inputRef.value, moment().format('DD.MM, HH:mm'));
+            let checkKey = (e.code === 'Enter' || e.code === 'NumpadEnter');
+            let checkVal = (this.inputRef.value !== '' && this.inputRef.value.trim() !== '' );
+            let message = { text: this.inputRef.value, time: moment().format('DD.MM, HH:mm') }
+
+            if ( checkKey && checkVal ) {
+                this.renderMessage(message.text, message.time);
+                this.saveMessage(message.text, message.time);
                 this.inputRef.value = '';
                 this.scrollChat();
             }
@@ -25,5 +33,18 @@ export default class MessageService {
 
     scrollChat() {
         document.getElementById('chat-box-container').scroll(0, this.chatRef.offsetHeight);
+    }
+
+    renderMessages() {
+        this.db.getData().then(data => {
+            for (let i in data) {
+                this.renderMessage(data[i].message, data[i].time);
+            }
+            this.scrollChat();
+        })
+    }
+
+    saveMessage(message, time) {
+        this.db.storeData({message: message, time: time});
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-    import IDBService from "./app/services/idb.service";
     import "./app/styles/style.css";
     import MessageService from "./app/services/message.service";
-
-    const db = new IDBService('newDB', 'werds');
     const messageService = new MessageService();
-
-    // if (window.Worker) {
-    //     const htmlCanvas = document.getElementById("canvas");
-    //     htmlCanvas.width = window.innerWidth;
-    //     htmlCanvas.height = window.innerHeight;
-
-    //     const offscreen = htmlCanvas.transferControlToOffscreen();
-
-    //     const worker = new Worker('./app/worker.js', {type: 'module'});
-    //     // const worker = new Worker(new URL('./app/worker.js', import.meta.url));
-    //     worker.postMessage({ canvas: offscreen }, [offscreen]);
-    // }
-
-
